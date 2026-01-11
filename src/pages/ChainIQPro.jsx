36b62e25ef4042c636e1ae753cb576fc513e1e70
PMCash <e051803.nwakusor@dlc.ui.edu.ng>
2026-01-11 21:27:55 +0100

    frontend update

--- src/pages/ChainIQPro.jsx
+++ src/pages/ChainIQPro.jsx
@@ -1,7 +1,7 @@
 export default function ChainIQPro() {
   return (
     <div style={{ padding: "30px" }}>
-      <h1>🚀 ChainIQ Pro</h1>
+      <h1> ChainIQ Pro</h1>
       <p>Welcome to premium analytics and tools.</p>
     </div>
   );
